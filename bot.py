a22f761a30cdbbcc71196db6e8096d6c87a3d990
katkovalchuk <kat.kovalchuk2016@gmail.com>
2020-03-25 18:50:55 +0200

    exception time finally fixed

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ api = tweepy.API(auth)
 
 import time
 import os
-
+"""
 def delete_old_tweets():
     for status in tweepy.Cursor(api.user_timeline).pages():
         i = 0
@@ -20,20 +20,20 @@ def delete_old_tweets():
                 api.destroy_status(status[i].id)
                 print("Deleted:", status[i].id)
             i = i + 1
-        
+"""        
 def wish_time():
     os.environ['TZ'] = 'Europe/Kiev'
     time.tzset()
     current_time = time.strftime("%H:%M")
-    splited_time = currentTime.split(":")
+    splited_time = current_time.split(":")
     
     perfect_time = ''
-    exceprion_time = ['01:01', '02:02', '03:03', '04:04', '05:05', '06:06', '07:07', '08:08', '09:09']
+    exception_time = ['01:01', '02:02', '03:03', '04:04', '05:05', '06:06', '07:07', '08:08', '09:09']
     
     if splited_time[0] == splited_time[1]:
         perfect_time = splited_time[0]+':'+ splited_time[1]
     
-    if perfect_time is True and perfect_time not in exception_time:
+    if bool(perfect_time) is True and perfect_time not in exception_time:
         api.update_status('It\'s ' + perfect_time +'! Make a wish!')
 
 while True:
